c2d282f19f50cf6f1e975da8f972604b3b231e69
Jason Wu <jsonwu34@gmail.com>
2023-09-15 13:50:41 -0400

    initalized project

---
+++ project_community_finder_backend/src/main/java/pcf/jjs/project_community_finder_backend/ProjectCommunityFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package pcf.jjs.project_community_finder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCommunityFinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCommunityFinderBackendApplication.class, args);
+	}
+
+}
---
+++ project_community_finder_backend/src/test/java/pcf/jjs/project_community_finder_backend/ProjectCommunityFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pcf.jjs.project_community_finder_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCommunityFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
