d6236d2476f13839be0d0bf3d2de0e96c470ae4b
Jason Wu <jsonwu34@gmail.com>
2023-09-18 17:21:02 -0400

    config setup to connect to postgres. ENV variables need to be setup on OS level

--- server/src/main/java/pcf/jjs/project_community_finder_server/ProjectCommunityFinderServerApplication.java
+++ server/src/main/java/pcf/jjs/project_community_finder_server/ProjectCommunityFinderServerApplication.java
@@ -3,6 +3,7 @@ package pcf.jjs.project_community_finder_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjectCommunityFinderServerApplication {
 
