768948ea0dd5ff2e83c8c6c93d0c008762aaefb9
Jason Wu <jsonwu34@gmail.com>
2023-09-19 17:13:41 -0400

    Added AccountErrorHandling File to handle error exceptions for validations and conflicts

--- server/src/main/java/pcf/jjs/project_community_finder_server/Account/AccountService.java
+++ server/src/main/java/pcf/jjs/project_community_finder_server/Account/AccountService.java
@@ -1,7 +1,16 @@
 package pcf.jjs.project_community_finder_server.Account;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Null;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -12,11 +21,18 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
     // Service for creating new accounts. Need to redo the error for when an account already exist
-    public void createAccount(Account account){
+    public Map<String, String> createAccount(@Valid Account account){
+
+//        Account newAccount = new Account(account.getUsername(), account.getPassword(), account.getEmail());
+
         Optional<Account> accountOptional = accountRepository.findAccountByEmail(account.getEmail());
         if(accountOptional.isPresent()){
-            throw new IllegalStateException("email taken");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+
         accountRepository.save(account);
+        Map<String, String> msgMap = new HashMap<>();
+        msgMap.put("message","Saved Successfully");
+        return msgMap;
     }
 }
