05076747d34dfab3c2ee6dbfcd0f7831d2b02b51
Jason <jsonwu34@gmail.com>
2023-10-27 17:46:54 -0400

    same error

--- server/src/main/java/pcf/jjs/project_community_finder_server/Auth0/SecurityConfiguration.java
+++ server/src/main/java/pcf/jjs/project_community_finder_server/Auth0/SecurityConfiguration.java
@@ -1,59 +1,59 @@
-package pcf.jjs.project_community_finder_server.Auth0;
+// package pcf.jjs.project_community_finder_server.Auth0;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
-public class SecurityConfiguration {
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+// import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+// import org.springframework.security.oauth2.jwt.Jwt;
+// import org.springframework.security.oauth2.jwt.JwtDecoder;
+// import org.springframework.security.oauth2.jwt.JwtDecoders;
+// import org.springframework.security.oauth2.jwt.JwtValidators;
+// import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+// import org.springframework.security.web.SecurityFilterChain;
 
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/auth0/public").permitAll()
-                        .requestMatchers("/auth0/private").hasAuthority("SCOPE_delete:friend_request")
-                        .requestMatchers("/api/friendRequest/**").authenticated()
-                        .requestMatchers("/api/account/**").authenticated()
-                        .requestMatchers("/api/friends/**").authenticated()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-    }
+// @Configuration
+// public class SecurityConfiguration {
 
 
-    @Value("${auth0.audience}")
-    private String audience;
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
-    }
+//         http
+//                 .cors(Customizer.withDefaults())
+//                 .authorizeHttpRequests((authorize) -> authorize
+//                         .requestMatchers("/auth0/public").permitAll()
+//                         .requestMatchers("/auth0/private").hasAuthority("SCOPE_delete:friend_request")
+//                         .requestMatchers("/api/friendRequest/**").authenticated()
+//                         .requestMatchers("/api/account/**").authenticated()
+//                         .requestMatchers("/api/friends/**").authenticated()
+//                 )
+//                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+//         return http.build();
+//     }
 
 
-}
+//     @Value("${auth0.audience}")
+//     private String audience;
+
+//     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//     private String issuer;
+
+//     @Bean
+//     JwtDecoder jwtDecoder() {
+//         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+
+//         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+//         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+//         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+//         jwtDecoder.setJwtValidator(withAudience);
+
+//         return jwtDecoder;
+//     }
+
+
+// }
