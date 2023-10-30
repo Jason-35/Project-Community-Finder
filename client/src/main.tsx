ccc0d8a70bd7b74801c38d29d07ff7476e35ffb6
Jaichyi Chen <jaichyichen@gmail.com>
2023-10-30 13:00:33 -0400

    saved main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
 import { BrowserRouter } from "react-router-dom";
 import Auth0ProviderWithNavigate from "./Auth0ProviderWithNavigate.tsx";
 
